02e5608416590d79fe58b2d91be76f5d79f2f9ce
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-09-07 11:44:13 +0300

    add input field and dropdown list for data input of lab1

---
+++ Models/Laba1VM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laba.Models
+{
+    public class Laba1VM
+    {
+        [Required]
+        public string ArrayString { get; set; }
+        [Required]
+        public TypeCode Type { get; set; }
+
+        public IEnumerable<SelectListItem> Types { get; set; } 
+        public Type[] Array { get; set; }
+        public IEnumerable<SortingAlgorithResultModel<string>> SortingAlgorithmStepsResult { get; set; }
+    }
+}
---
+++ Models/SortingAlgorithStepResultModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laba.Models
+{
+    public class SortingAlgorithResultModel<T>
+    {
+        [Required]
+        public IEnumerable<T> Array { get; set; }
+
+        public int Index1ToSwap { get; set; }
+        public int Index2ToSwap { get; set; }
+    }
+}
