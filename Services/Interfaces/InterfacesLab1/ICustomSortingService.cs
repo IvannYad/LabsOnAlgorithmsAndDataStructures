07bce5e343a1c3081b8db8f42b5970a9e524c761
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-09-22 20:52:17 +0300

    feat: add interfaces for sorting in lab1

---
+++ Services/Interfaces/InterfacesLab1/ICustomSortingService.cs
@@ -0,0 +1,8 @@
+﻿using Laba.Models;
+
+namespace Laba.Services.Interfaces.InterfacesLab1
+{
+    public interface ICustomSortingService: ICustomSorting<IEnumerable<string>, List<SortingAlgorithmStepResultModelLab1>>, IFindMin<string[]>, ISwap<string>
+    {
+    }
+}
