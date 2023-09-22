6c0b516277810474f3ac409589f88dc3c785525f
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-09-22 19:52:52 +0300

    feat: add ISorting, ICustomSorting and ISwap interfaces

---
+++ Services/Interfaces/ISorting.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace Laba.Services.Interfaces
+{
+    public interface ISorting<TIn, TOut> where TIn : IEnumerable
+    {
+        TOut Sort(ref TIn input);
+    }
+}
---
+++ Services/Interfaces/ISwap.cs
@@ -0,0 +1,7 @@
+﻿namespace Laba.Services.Interfaces
+{
+    public interface ISwap<T>
+    {
+        void Swap(ref T a, ref T b);
+    }
+}
