e0e33d0d51e5b7505ef08de6ae2749494ee46ee9
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-10-01 14:17:10 +0300

    feat: create SortingAlgorithmStepResultModelLab5

---
+++ Models/SortingAlgorithmStepResultModelLab5.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Laba.Models
+{
+    public class SortingAlgorithmStepResultModelLab5
+    {
+        [Required]
+        public double[] Array { get; set; }
+
+        public int CurrentIndex { get; set; }
+    }
+}
