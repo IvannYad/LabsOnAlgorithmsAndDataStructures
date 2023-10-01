41bfd310f99cd2951dc2fd2c81159bb2b764989f
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-10-01 18:46:08 +0300

    feat: make PrepareCollectionServiceLab5 proper

--- Services/ServicesLab5/PrepareCollectionServiceLab5.cs
+++ Services/ServicesLab5/PrepareCollectionServiceLab5.cs
@@ -13,18 +13,11 @@ namespace Laba.Services.ServicesLab5
             var intList = inputCollection
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select((e, i) => i % 2 == 0 ? Math.Sqrt(Math.Abs(double.Parse(e) - 10)) : double.Parse(e))
-                .Select(d => (int)(d * 1000000))
+                .Select(d => (int)(d * 100))
                 .ToList();
-
-            if (intList.Any(i => i < 0 || i > 10))
-            {
-                throw new ArgumentException();
-            }
-
+        
             if (intList.Count <= 2)
                 return new int[0];
-
-            
 
             return intList.ToArray();
         }
