5a6375547fc79648fc374e504c61921958c58415
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-10-01 14:15:13 +0300

    feat: create IOrdinarySortingService5; add ICustomSortingService5

---
+++ Services/Interfaces/InterfacesLab5/ICustomSortingService5.cs
@@ -0,0 +1,12 @@
+﻿using Laba.Models;
+using Laba.Services.Interfaces.SortingInterfaces;
+using System.Runtime.InteropServices;
+
+namespace Laba.Services.Interfaces.InterfacesLab5
+{
+    public interface ICustomSortingService5 : ISorting<int[], int>, ISwap<int>
+    {
+        public List<SortingAlgorithmStepResultModelLab4> Steps { get; }
+        public int ComparesCount { get; }
+    }
+}
---
+++ Services/Interfaces/InterfacesLab5/IOrdinarySortingService5.cs
@@ -0,0 +1,8 @@
+﻿using Laba.Services.Interfaces.SortingInterfaces;
+
+namespace Laba.Services.Interfaces.InterfacesLab5
+{
+    public interface IOrdinarySortingService5 : ISorting<int[], int>, ISwap<int>
+    {
+    }
+}
