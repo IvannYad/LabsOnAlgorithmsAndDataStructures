82e5deba646fa7e56344f63bdf9e770f784df590
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-10-16 16:56:50 +0300

    feat: create Lab6VM

---
+++ Models/VM/Lab6VM.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laba.Models.VM
+{
+    public class Lab6VM
+    {
+        [Required]
+        public int NumberOfElementsString { get; set; }
+        public int[] NumberOfElements { get; set; }
+        [Required]
+        [Range(1, 1000)]
+        public int LowerBound { get; set; }
+        [Required]
+        [Range(1, 1000)]
+        public int UpperBound { get; set; }
+        
+        public bool IsDataPopulated { get; set; }
+        
+        public double[] SelectionSortTimes { get; set; }
+        public double[] ShellSortTimes { get; set; }
+        public double[] QuickSortTimes { get; set; }
+        public double[] MergeSortTimes { get; set; }
+        public double[] CountSortTimes { get; set; }
+    }
+}
