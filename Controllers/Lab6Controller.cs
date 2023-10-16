98b628b6f6c515b00403a40f5b81eb4a7904dd46
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-10-16 16:49:47 +0300

    feat: create Lab6Controller; create View for Index() of Lab6Controller

---
+++ Controllers/Lab6Controller.cs
@@ -0,0 +1,25 @@
+﻿using Laba.Models.VM;
+using Laba.Services.Interfaces.InterfacesLab3;
+using Laba.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Laba.Services.Interfaces.InterfacesLab4;
+using Laba.Services.ServicesLab4;
+using Laba.Services.Interfaces.InterfacesLab5;
+using Laba.Services.ServicesLab5;
+
+
+namespace Laba.Controllers
+{
+    public class Lab6Controller : Controller
+    {
+        
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            return View(new int[] { 150, 200, 300, 400, 500, 600, 700, 800, 900, 1000 });
+        }
+
+    }
+}
