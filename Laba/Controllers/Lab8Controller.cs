901833c4ea4100063e94d44d4b768ab2053db606
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-04 16:39:40 +0200

    feat: inplement Index View

---
+++ Laba/Controllers/Lab8Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laba.Controllers
+{
+    public class Lab8Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
