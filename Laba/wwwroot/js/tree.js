ca5a6cd8076d3d220c8b33d20875d6458374a97d
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-04 18:43:18 +0200

    feat: add code to tree.js

--- Laba/wwwroot/js/tree.js
+++ Laba/wwwroot/js/tree.js
@@ -1,2 +1,12 @@
-﻿
+﻿function DrawTree(arr) {
+    let targetFn = treeVisualizer(
+        { target: "targetDiv" }
+    )
+    targetFn.drawData(
+        [{
+            data: "[1,-5,15,-9,-4,10,17,null,-6,null,0,null,14, 12, 22, 34, null]"
+        }]
+    )
+
+}
 
