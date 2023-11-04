c7268f1d1d0b76b409193fcdeb3e01337a1a1a01
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-04 18:54:42 +0200

    feat: define ITree interface; create TreeNode class

---
+++ Laba/DataStructures/Interfaces/ITree.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Laba.DataStructures.Interfaces
+{
+    public interface ITree
+    {
+        void Add(double value);
+        double[] GetTraversing();
+        bool IfExists(double value);
+        (double parent, double[]? children) GetParentAndChildren();
+        string GetPackedArray();
+    }
+}
---
+++ Laba/DataStructures/TreeNode.cs
@@ -0,0 +1,10 @@
+﻿namespace Laba.DataStructures
+{
+    public class TreeNode
+    {
+        public double Value { get; set; }
+        public TreeNode? Left { get; set; }
+        public TreeNode? Right { get; set; }
+        public TreeNode Parent { get; set; }
+    }
+}
