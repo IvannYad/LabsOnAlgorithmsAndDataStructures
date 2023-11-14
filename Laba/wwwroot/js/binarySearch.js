7bf1af7082a9064c373eac235effa86b9b970324
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-14 21:13:04 +0200

    feat: create javascript for binary search

---
+++ Laba/wwwroot/js/binarySearch.js
@@ -0,0 +1,19 @@
+﻿function DrawGistogram(labels, colors, data) {
+    return new Chart(elements, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [{
+                backgroundColor: colors,
+                data: data
+            }]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+                display: false,
+                text: ""
+            }
+        }
+    });
+}
