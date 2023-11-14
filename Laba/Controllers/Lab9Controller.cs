fe6c22100a350947083ebeb2b01adc2ea9b770b3
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-14 17:01:20 +0200

    feat: creade Lab9Controller; create Index View for Lab9Controller; add link to Lab9Controller to navbar

---
+++ Laba/Controllers/Lab9Controller.cs
@@ -0,0 +1,21 @@
+﻿using Laba.DataStructures;
+using Laba.DataStructures.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using Microsoft.JSInterop;
+
+namespace Laba.Controllers
+{
+    public class Lab9Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API_Calls
+        
+        #endregion
+    }
+}
