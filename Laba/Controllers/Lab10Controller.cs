08e15ce583e27c08c83f3ac83cfbeddfab53d963
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-20 19:33:30 +0200

    feat: implement text generation with Lorem Ipsum generator

--- Laba/Controllers/Lab10Controller.cs
+++ Laba/Controllers/Lab10Controller.cs
@@ -4,11 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 using System.Runtime.CompilerServices;
 using Microsoft.JSInterop;
 using Laba.Services.ServicesLab9;
+using System.Net;
 
 namespace Laba.Controllers
 {
     public class Lab10Controller : Controller
     {
+        private string? _text1;
+        private string? _text2;
         public IActionResult Index()
         {
             return View();
@@ -16,7 +19,19 @@ namespace Laba.Controllers
 
         #region API_Calls
 
-        
+        public async Task<IActionResult> GenerateText(int paragraphsCount, string textArea)
+        {
+            HttpClient client = new();
+            string text = await client.GetStringAsync($"https://loripsum.net/api/{paragraphsCount}/short");
+            if (textArea == "textArea1")
+            {
+                _text1 = WebUtility.HtmlDecode(text);
+                return Json(_text1);
+            }
+
+            _text2 = WebUtility.HtmlDecode(text);
+            return Json(_text2);
+        }
 
         #endregion
     }
