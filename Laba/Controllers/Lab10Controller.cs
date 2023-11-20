09285620c3d17233ebfdd88924ce2705b56ac91b
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-20 17:39:05 +0200

    feat: lab10 initial create

---
+++ Laba/Controllers/Lab10Controller.cs
@@ -0,0 +1,23 @@
+﻿using Laba.DataStructures;
+using Laba.DataStructures.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using Microsoft.JSInterop;
+using Laba.Services.ServicesLab9;
+
+namespace Laba.Controllers
+{
+    public class Lab10Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API_Calls
+
+        
+
+        #endregion
+    }
+}
